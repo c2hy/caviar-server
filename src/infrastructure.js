25644e4202c547e00f5b99bf5c0bd2634e4a1c83
c2hy <c2hy@hotmail.com>
2021-04-17 22:05:44 +0800

    使用低版本的redis

--- src/infrastructure.js
+++ src/infrastructure.js
@@ -1,3 +1,3 @@
-const { ClientV3: Client } = require("@camaro/redis");
+const { ClientV2: Client } = require("@camaro/redis");
 const reidsConfig = process.env.NODE_ENV === "prod" ? { password: "suzor" } : { password: "123456" }
 exports.redisClient = new Client(reidsConfig);
