360b39f14e77223828974bbbce03e18413293829
c2hy <c2hy@hotmail.com>
2021-04-18 00:32:53 +0800

    解决中文乱码问题

--- src/infrastructure.js
+++ src/infrastructure.js
@@ -1,3 +1,17 @@
-const { ClientV2: Client } = require("@camaro/redis");
-const reidsConfig = process.env.NODE_ENV === "prod" ? { password: process.env.REDIS_PASSWORD } : { password: "123456" }
-exports.redisClient = new Client(reidsConfig);
+const redis = require("redis");
+const { promisify } = require("util");
+const client = redis.createClient(process.env.NODE_ENV === "prod" ? { password: process.env.REDIS_PASSWORD } : { password: "123456" })
+client.on("error", function (error) {
+    console.error(error);
+});
+
+exports.redisClient = {
+    SET: promisify(client.SET).bind(client),
+    GET: promisify(client.GET).bind(client),
+    ZADD: promisify(client.ZADD).bind(client),
+    ZREM: promisify(client.ZREM).bind(client),
+    SADD: promisify(client.SADD).bind(client),
+    SREM: promisify(client.SREM).bind(client),
+    ZREVRANGE: promisify(client.ZREVRANGE).bind(client),
+    SMEMBERS: promisify(client.SMEMBERS).bind(client),
+};
--- test/draft.test.js
+++ test/draft.test.js
@@ -3,9 +3,8 @@ const { DateTime } = require("luxon");
 const { v4: uuid } = require('uuid');
 
 async function test() {
-    const momentId = uuid();
-    const createdTime = DateTime.now().toMillis();
-    await redisClient.SET(`moment:${momentId}`, JSON.stringify({ momentId, content:"尝试插入中文的内容", createdTime }))
+    const list = []
+    list.push({lst:"123"})
 }
 
 test()
