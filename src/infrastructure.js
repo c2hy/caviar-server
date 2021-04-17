b200fcd9c2230295506ff5db50a317c57a23c15b
c2hy <c2hy@hotmail.com>
2021-04-17 23:01:49 +0800

    环境变量保持密码

--- src/infrastructure.js
+++ src/infrastructure.js
@@ -1,3 +1,3 @@
 const { ClientV2: Client } = require("@camaro/redis");
-const reidsConfig = process.env.NODE_ENV === "prod" ? { password: "suzor" } : { password: "123456" }
+const reidsConfig = process.env.NODE_ENV === "prod" ? { password: process.env.REDIS_PASSWORD } : { password: "123456" }
 exports.redisClient = new Client(reidsConfig);
